d6c0584222a37e340d813bfdafd0c25846746178
Hank, Liang <ping2291@hotmail.com>
2017-03-28 23:12:09 +0800

    修正查詢匯率規則

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var linebot = require('linebot');
 var express = require('express');
-var cheerio = require('cheerio');
-var request = require('request');
-var allRate = {};//暫存所有匯率
+var rate = require('./src/query_rate');
+const updateDuringTime = 1000*60*60; //每一小時更新一次匯率
+
 var bot = linebot({
   channelId: '1507809147',
   channelSecret: '19abedc7a873bd705cf76bdb06f8faaf',
@@ -10,11 +10,11 @@ var bot = linebot({
 });
 
 bot.on('message', function (event) {
-  if(/日幣|日圓|JPY|jpy|¥/g.test(event.message.text)){
+  if(/^$/g.test(event.message.text)){
     //resquest('http://rate.bot.com.tw/xrt?Lang=zh-TW');
-    reply(event, '日圓現金買入匯率為 : ' + allRate['日圓 (JPY)']);
+    event.reply(rate.queryRate(event.message.text));
   }else{
-    reply(event, event.message.text);
+    event.reply(event, event.message.text);
   }
 });
 
@@ -23,7 +23,7 @@ const linebotParser = bot.parser();
 //line Basic information => Webhook URL https://mylinebotv1.herokuapp.com/linewebhook
 app.post('/linewebhook', linebotParser);
 // app.get('/query', function(req, res){
-//   queryRate();
+//   res.send('quert get!');
 // })
 //app.listen(3000);
 
@@ -31,40 +31,7 @@ app.post('/linewebhook', linebotParser);
 var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
   console.log("App now running on port : ", port);
-  queryRate();//抓取匯率
+  //每1小時抓取一次匯率
+  rate.updateRate();
+  setInterval(rate.updateRate, updateDuringTime);
 });
-
-function reply(e, msg){
-  e.reply(msg).then(function(data){
-    //success
-  }).catch(function(error){
-    //error
-    console.log('error : ', error);
-  });
-}
-
-function queryRate(){
-  request('http://rate.bot.com.tw/xrt?Lang=zh-TW', function(error, response, body){
-    if(error){
-      console.log('error:', error); // Print the error if one occurred
-    }
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // console.log('body:', body); // Print the HTML for the Google homepage.
-    var $ = cheerio.load(body)
-    // var content = '日圓現金買入匯率為 : ';
-    //幣別從第三個開始
-    var rateList = $('div.print_show');
-    // var index = 0;
-
-    rateList.each(function(i,e){
-      allRate[$(this).text().trim()] = $($('.rate-content-cash[data-table=本行現金買入]')[i]).text();
-      // if(.includes('日圓')){
-      //   // console.log(i, $(this).text().trim());
-      //   index = i;
-      // }
-    });
-    console.log('allRate : ', allRate);
-    // console.log($($('.rate-content-cash[data-table=本行現金買入]')[index]).text());
-    // content += $($('.rate-content-cash[data-table=本行現金買入]')[index]).text();
-  });
-}
---
+++ src/query_rate.js
@@ -0,0 +1,45 @@
+var cheerio = require('cheerio');
+var request = require('request');
+
+const rateRules = [/美金|美元|美國|USD|usd/g, /港幣|香港|HKD|hkd/g, /英鎊|英國|GBP|gbp/g, /澳幣|澳洲|AUD|aud/g, /日圓|日幣|日本|JPY|jpy/g];
+const rateTypes = ['美金', '港幣', '英鎊', '澳幣', '日圓']
+var allRate = {};//暫存所有匯率
+
+var requestOptions = {
+  url: '',
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+  }
+}
+
+
+function updateRate(){
+  requestOptions.url = 'http://rate.bot.com.tw/xrt?Lang=zh-TW';
+  request(requestOptions, function(error, response, body){
+    if(error){
+      console.log('error:', error); // Print the error if one occurred
+    }
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+    var $ = cheerio.load(body);
+    var rateList = $('div.print_show');
+
+    rateList.each(function(i,e){
+      allRate[$(this).text().trim().split(' ')[0]] = $($('.rate-content-cash[data-table=本行現金買入]')[i]).text();
+    });
+    console.log('allRate : ', allRate);
+  });
+}
+
+function queryRate(queryType){
+  for(let i = 0 ; i < rateRules.length ; i++){
+    if(rateRules[i].test(queryType)){
+      return allRate[rateTypes[i]];
+    }
+  }
+}
+
+module.exports = {
+  updateRate: updateRate,
+  queryRate: queryRate
+};
