04d4c057e5e6720215c773a376623be33f2b3ba9
unknown <>
2017-03-28 15:03:46 +0800

    change channelId

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ var linebot = require('linebot');
 var express = require('express');
 
 var bot = linebot({
-  channelId: 1507809147,
+  channelId: '1507809147',
   channelSecret: '19abedc7a873bd705cf76bdb06f8faaf',
   channelAccessToken: '7CNicd1iAoHlllHo1peevvAZMqcSMedV3PE6qTyeBGvGx9OYDoqo3bLDTQm66eInjXUvGgmBf5Fevx7MIOpTzq0kE8HFFXvc51YVJeUhEyIMTuCxAeSs2dAs0nzM/sHjzFw7SA5QJfMgyZZjODDWvQdB04t89/1O/w1cDnyilFU='
 });
 
-
-bot.on('message', function(e){
+bot.on('message', function (event) {
   console.log('message : ' + e);
+  event.reply(event.message.text).then(function (data) {
+      // success
+      console.log('success : ' + data);
+  }).catch(function (error) {
+      // error
+      console.log('error : ' + error);
+  });
 });
 
 const app = express();
